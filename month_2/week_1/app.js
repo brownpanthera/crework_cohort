71ac87337b897f73eae4e9981b59b88d9b25d4b5
Ali Faizan <68064523+brownpanthera@users.noreply.github.com>
2022-08-19 13:22:02 +0530

    Update app.js

--- month_2/week_1/app.js
+++ month_2/week_1/app.js
@@ -1,15 +1,25 @@
 let btn = document.getElementById("btn");
 let text = document.getElementById('txt');
+let span = document.querySelector('.span');
 
+
+// Color Function
 function getRandomColor() {
   let letters = "0123456789ABCDEF";
   let color = "#";
   for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
-
-  text.innerText = 'Background Color ' + color;
+  span.innerText =  color;
   // console.log(color);
   document.body.style.backgroundColor = color;
 }
+
 btn.addEventListener("click", getRandomColor);
+
+// Audio Function
+function audio(){
+  let music = new Audio('/audio/waka.mp3');
+  music.play();
+}
+btn.addEventListener('click', audio);
