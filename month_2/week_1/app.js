ee54adf2ae9b925a1a7c138a9c1ac0f896f3679b
Ali Faizan <68064523+brownpanthera@users.noreply.github.com>
2022-08-20 11:30:06 +0530

    added previous color btn functionality

--- month_2/week_1/app.js
+++ month_2/week_1/app.js
@@ -2,6 +2,11 @@ let btn = document.getElementById("btn");
 let text = document.getElementById("txt");
 let span = document.querySelector(".span");
 
+let preTxt = document.querySelector(".span");
+let preBtn = document.getElementById("pre-btn");
+
+let arr = [];
+
 // Color Function
 function getRandomColor() {
   let letters = "0123456789ABCDEF";
@@ -9,11 +14,13 @@ function getRandomColor() {
   for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
+
+  // pushing generated color into array
+  arr.push(color);
+
   span.innerText = color;
-  // console.log(color);
   document.body.style.backgroundColor = color;
 }
-
 btn.addEventListener("click", getRandomColor);
 
 // Audio Function
@@ -22,3 +29,22 @@ function audio() {
   music.play();
 }
 btn.addEventListener("click", audio);
+
+
+// previous color fuction
+function previousColor() {
+  for (let i = 0; i < arr.length; i++) {
+    back = arr[i - 1];
+  }
+  preTxt.innerText = back;
+  document.body.style.backgroundColor = back;
+}
+preBtn.addEventListener("click", previousColor);
+
+
+// previous color audio
+function Preaudio(){
+  let prevMusic = new Audio("/audio/ironmanSnap.mp3");
+  prevMusic.play();
+}
+preBtn.addEventListener("click", Preaudio);
